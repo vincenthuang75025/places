010b0c51630162deb011499f11b1e8eb1d78771d
io0 <marleyxiong0@gmail.com>
2024-01-21 12:03:42 -0800

    table with users

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,20 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { Suspense } from 'react'
-import Table from '@/components/table'
-import TablePlaceholder from '@/components/table-placeholder'
-import ExpandingArrow from '@/components/expanding-arrow'
-import { PrismaClient, Prisma } from '@prisma/client'
+import Image from "next/image";
+import Link from "next/link";
+import { Suspense } from "react";
+import Table from "@/components/table";
+import TablePlaceholder from "@/components/table-placeholder";
+import ExpandingArrow from "@/components/expanding-arrow";
+import { PrismaClient, Prisma } from "@prisma/client";
 
-const prisma = new PrismaClient()
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
 export default function Home() {
-  async function createRecords() {
-    'use server';
-    const user = await prisma.user.create({
-    data: {
-      email: 'aayushgupta05@gmail.com',
-      name: 'Aayush Gupta',
-      image: "",
-    },
-  })
-}
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-center">
-      <button onClick={createRecords}>Press</button>
-    </main>
-  )
+    <div className="relative flex min-h-screen flex-col items-center justify-center">
+      <button>Press</button>
+      <Suspense fallback={<TablePlaceholder />}>
+        <Table />
+      </Suspense>
+    </div>
+  );
 }
