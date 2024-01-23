816004db9fbb75d74740457d131d3d8d2469944b
Vincent Huang <vincenthuang75025@yahoo.com>
2024-01-23 08:41:43 -0800

    figure out how to get posting to work

--- app/temp_name/page.tsx
+++ app/temp_name/page.tsx
@@ -5,9 +5,7 @@ import '../../components/styles.scss';
 
 import { useState } from 'react';
 
-import { PrismaClient, Prisma } from '@prisma/client'
-const prisma = new PrismaClient()
-export const dynamic = 'force-dynamic'
+import postVisit from "./temp";
 
 const Page2 = () => {
 
@@ -21,38 +19,12 @@ const Page2 = () => {
   }
 
   const submitVisit = () => {
-    console.log(startDate, endDate, location)
-
-    // const visit = prisma.visit.create({
-    //   data: {
-    //     location: location,
-    //     start: startDate,
-    //     end: endDate,
-    //     userId: 1,
-    //     // todo: how to get user id?
-    //   },
-    // }).then(m => console.log(m));
-
-    const postData = async () => {
-      const data = {
-        title: 1,
-        post: 2,
-      };
-
-      const response = await fetch("/api/createVisit", {
-        method: "POST",
-        body: JSON.stringify(data),
-      });
-      return response.json();
-    };
-    postData().then((data) => {
-      alert(data.message);
-    });
+    postVisit(startDate, endDate, location)
   }
 
   return (<>
-    {/* input box for city name */}
-    <input type="text" placeholder="location" onChange={(e) => setLocation(e.target.value)} />
+  <div className="flex flex-row">
+    <input className="w-1/4" type="text" placeholder="location" onChange={(e) => setLocation(e.target.value)} />
     <RangeDatePicker
       startDate={startDate}
       endDate={endDate}
@@ -64,10 +36,10 @@ const Page2 = () => {
       startDatePlaceholder="Start Date"
       endDatePlaceholder="End Date"
       disabled={false}
-      className="my-own-class-name"
+      className="w-1/2"
       startWeekDay="monday"
     />
-    {/* button to post start and end dates */}
+  </div>
     <button onClick={submitVisit}>Click</button>
   </>)
 }
